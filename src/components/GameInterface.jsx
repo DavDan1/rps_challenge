eba2b92542594281e7de4e5443828d63bc9b11b4
DavDan1 <david-danielyan@hotmail.com>
2021-04-28 23:14:27 +0200

    add test for buttons, adds function onClick for buttons

--- src/components/GameInterface.jsx
+++ src/components/GameInterface.jsx
@@ -1,15 +1,35 @@
-import React, { Component } from 'react'
-import { Button } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
 
+const items = ["Rock", "Paper", "Scissors"];
 class GameInterface extends Component {
+
+
+ 
+  
+  selectItem = (item) => {
+    this.setState({
+      player: item,
+      message: "",
+    });
+  };
   render() {
     return (
       <div>
-        <Button data-cy="rock-button">Rock</Button>
-        <Button data-cy="paper-button">Paper</Button>
-        <Button data-cy="scissors-button">Scissors</Button>
+        <Button data-cy="rock-button" onClick={() => this.selectItem("Rock")}>
+          Rock
+        </Button>
+        <Button data-cy="paper-button" onClick={() => this.selectItem("Paper")}>
+          Paper
+        </Button>
+        <Button
+          data-cy="scissors-button"
+          onClick={() => this.selectItem("Scissors")}
+        >
+          Scissors
+        </Button>
       </div>
-    )
+    );
   }
 }
-export default GameInterface
+export default GameInterface;
